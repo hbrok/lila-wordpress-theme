3a56d5b806139c1e10921095f2c7a92889f0a488
hbrok <heather.brokmeier@gmail.com>
2015-07-28 18:52:25 -0500

    Update heading and paragraph styles
    Removed too much space before paragraphs and centered headings
    on page-projects.php

--- page-projects.php
+++ page-projects.php
@@ -2,7 +2,7 @@
 
   <main class="wrapper">
       <div class="row">
-        <h1><?php echo wp_title(''); ?></h1>
+        <h1 class="fancy-heading">Projects</h1>
 
     	  <?php
 
@@ -17,7 +17,7 @@
 
 				<div class="col-4">
         	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-        	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        	<h3 class="project-heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
       	</div>
 
         <?php	endwhile; ?>
